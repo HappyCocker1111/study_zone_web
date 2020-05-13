c63988a44e0d71015eaefb6b6457777e074cd01a
弥和 <a.cokegasuki@gmail.com>
2020-05-13 10:13:22 +0900

    add user todo list

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,10 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+
+  resources :users, only: [] do
+    resources :tasks, only: :index
+  end
+
+  resources :tasks, only: %i[create update]
 end
--- db/migrate/20200512063735_create_users.rb
+++ db/migrate/20200512063735_create_users.rb
@@ -1,5 +1,4 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
-  has_many :tasks
   def change
     create_table :users do |t|
       t.string :name
